2de0b8d62458a5ca2ae5d9490c452e0872221d24
vishalxbhargav <vishalxbhargav@gmail.com>
2025-01-30 19:20:51 +0530

    Revert "Setup Smtp server using mailersend"
    This reverts commit 63cd1f39b881c860d48989621594b40fd419a077.

--- app/mailers/course_mailer.rb
+++
@@ -1,6 +0,0 @@
-class CourseMailer < ApplicationMailer
-    default from: 'MS_JerSGC@trial-vywj2lprk0m47oqz.mlsender.net' # this domain must be verified on your MailerSend dashboard
-    def welcome_email
-        mail(to: ["vishalxbhargav@gmail.com"], subject: 'Hello from MailerSend!')
-    end
-end
--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {  
-      :address              => "smtp.mailersend.net",  
-      :port                 => 587,  
-      :domain               => "trial-vywj2lprk0m47oqz.mlsender.net",  
-     :user_name            => "MS_JerSGC@trial-vywj2lprk0m47oqz.mlsender.net",
-      :password             => "mssp.34SrKPd.vywj2lpxrwk47oqz.yc0K1OV",
-      :authentication       => "plain",  
-      :enable_starttls_auto => true  
-}
