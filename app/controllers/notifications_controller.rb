633c7644565e04b4b0d64e15836c87b8d15e47fe
vishalxbhargav <vishalxbhargav@gmail.com>
2025-01-31 14:48:14 +0530

    update read all notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,15 +1,13 @@
 class NotificationsController < ApplicationController
     include NotificationsHelper
     before_action :authenticate_user!
-    skip_before_action :verify_authenticity_token
-
     def index
         @notifications=current_user.unread_notification
     end
     def all_read
         @notifications=current_user.unread_notification
         if read_all(@notifications)
-            render plain: "all notification read", status: 200
+            redirect_to root_path
         else
             render plain: "something went wrong",status: 500
         end
