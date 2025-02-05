5003516a7d081379e511faffc81f82aa2e3a0b81
vishalxbhargav <vishalxbhargav@gmail.com>
2025-02-05 17:04:50 +0530

    add pagination using pagy gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  include Pagy::Backend
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
