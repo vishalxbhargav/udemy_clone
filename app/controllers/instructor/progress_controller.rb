0642fce705ef23bae701591c5546eee40acb5f58
vishalxbhargav <vishalxbhargav@gmail.com>
2025-02-11 16:33:16 +0530

    update progress bar authorization

--- app/controllers/instructor/progress_controller.rb
+++ app/controllers/instructor/progress_controller.rb
@@ -1,5 +1,4 @@
 class Instructor::ProgressController < ApplicationController
-    load_and_authorize_resource
     before_action :authenticate_user!
     skip_before_action :verify_authenticity_token
 
